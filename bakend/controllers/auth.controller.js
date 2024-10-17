b77cc382604a52dae5ca2a84bae1b8460bf3ecdb
mariamkhan3300 <mariamkhan33000>
2024-10-17 15:36:50 +0500

    Project Setup

---
+++ bakend/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export const signup = async (req, res) => {
+    res.json({
+        data: "You can hit on signup"
+    })
+}
+export const login = async (req, res) => {
+    res.json({
+        data: "You can hit on signup"
+    })
+}
+export const logout = async (req, res) => {
+    res.json({
+        data: "You can hit on signup"
+    })
+}
---
+++ bakend/server.js
@@ -0,0 +1,18 @@
+import express from "express"
+import authRoutes from './routes/auth.routes.js'
+
+import dotenv from 'dotenv'
+import connectMongoDB from "./db/connectMongoDB.js";
+dotenv.config()
+
+const app = express();
+const PORT = process.env.PORT || 5000
+
+console.log(process.env.MONGO_LOCAL)
+app.use("/api/auth", authRoutes)
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on the port ${PORT}`)
+    connectMongoDB()
+})
