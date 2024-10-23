c1c224fc59a072e7b9ad7d4aba0b3099971e0e37
mariamkhan3300 <mariamkhan33000>
2024-10-23 10:27:28 +0500

    3-Auth handled and Routes Protected in Client

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,22 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
+const queryClientInstance = new QueryClient({
+  defaultOptions : {
+    queries : {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <QueryClientProvider client={ queryClientInstance }>
     <App />
+    </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 )
